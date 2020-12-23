bb51e0c424b9d2a4ebf5b6835171f38bde58e613
nathanramnath21 <71851194+nathanramnath21@users.noreply.github.com>
2020-12-23 17:11:40 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -4,12 +4,15 @@ var score = 0;
 var bgImg1;
 var gameState= "begin";
 var car1, car2, car3, car4;
+var random;
 
 function preload(){
     turtleImg = loadImage("Images/turtle.png");
     bgImg1 = loadImage("Images/road.png")
     car1 = loadImage("Images/car1.png")
+
     car2 = loadImage("Images/car2.png")
+    car2.scale= 0.5;
     car3 = loadImage("Images/car3.png")
     car4 = loadImage("Images/car4.png")
 
@@ -32,17 +35,9 @@ function draw(){
     if(gameState === "begin"){
         
         turtleMovement();
-        if (turtle.y<600){
-            gameState === "play"
-        }
+        spawnCars();
     }
         
-        if(gameState === "play")
-        {
-            spawnCars();
-        }
-        
-
     drawSprites();
 }   
 
@@ -71,13 +66,27 @@ function turtleMovement(){
     if (keyWentUp(RIGHT_ARROW)) {
         turtle.velocityX=0;
     }
-    
+    drawSprites();
 }
 
 function spawnCars(){
 
         if (frameCount %80===0){
-            var car = createSprite(100,100,)
+            var car = createSprite(displayWidth/8,displayHeight/8,10,10)
+            car.y = Math.round(random(800,25))
+            
+            var rand = Math.round(random(1,6));
+    switch(rand) {
+      case 1:car.addImage(car1);
+              break;
+      case 2:car.addImage(car2);
+              break;
+      case 3:car.addImage(car3);
+              break;
+      case 4:car.addImage(car4);
+              break;
+      default: break;
             
         }
+}
 }
